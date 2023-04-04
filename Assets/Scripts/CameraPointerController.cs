4f164ade917fa0eee34618082a628fc74a7bcd28
IveTou <ive_tourinho@hotmail.com>
2023-04-04 20:02:54 -0300

    feat: setup network based movement

--- Assets/Scripts/CameraPointerController.cs
+++ Assets/Scripts/CameraPointerController.cs
@@ -1,8 +1,9 @@
+using Mirror;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CameraPointerController : MonoBehaviour
+public class CameraPointerController : NetworkBehaviour
 {
     private const float _maxDistance = 10;
     private GameObject _gazedAtObject = null;
@@ -21,55 +22,58 @@ public class CameraPointerController : MonoBehaviour
 
     public void Update()
     {
-        // Casts ray towards camera's forward direction, to detect if a GameObject is being gazed
-        // at.
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, _maxDistance))
-        {            
-            // GameObject detected in front of the camera.
-            if (_gazedAtObject != hit.transform.gameObject)
-            {
-                // New GameObject.
-                _gazedAtObject?.SendMessage("OnPointerExit");
-                _gazedAtObject = hit.transform.gameObject;
-                _gazedAtObject.SendMessage("OnPointerEnter");
-
-                if( _gazedAtObject.tag != "Environment")
+        if (isLocalPlayer)
+        {
+            // Casts ray towards camera's forward direction, to detect if a GameObject is being gazed
+            // at.
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, _maxDistance))
+            {            
+                // GameObject detected in front of the camera.
+                if (_gazedAtObject != hit.transform.gameObject)
                 {
-                    hasPointerEntered = true;
-                } else {
-                    hasPointerEntered = false;
+                    // New GameObject.
+                    _gazedAtObject?.SendMessage("OnPointerExit");
+                    _gazedAtObject = hit.transform.gameObject;
+                    _gazedAtObject.SendMessage("OnPointerEnter");
+
+                    if( _gazedAtObject.tag != "Environment")
+                    {
+                        hasPointerEntered = true;
+                    } else {
+                        hasPointerEntered = false;
+                    }
                 }
             }
-        }
-        else
-        {
-            // No GameObject detected in front of the camera.
-            _gazedAtObject?.SendMessage("OnPointerExit");
-            _gazedAtObject = null;
-
-            hasPointerEntered = false;
-        }
-
-        if (hasPointerEntered)
-        {
-            onPointerEnterCounter += Time.deltaTime;
-
-            Loader.SetActive(true);
-            SliderObject.value = onPointerEnterCounter / onPointerClickTime;
-
-            if (onPointerEnterCounter >= onPointerClickTime)
+            else
             {
-                _gazedAtObject?.SendMessage("OnPointerClick");
+                // No GameObject detected in front of the camera.
+                _gazedAtObject?.SendMessage("OnPointerExit");
+                _gazedAtObject = null;
+
+                hasPointerEntered = false;
+            }
+
+            if (hasPointerEntered)
+            {
+                onPointerEnterCounter += Time.deltaTime;
+
+                Loader.SetActive(true);
+                SliderObject.value = onPointerEnterCounter / onPointerClickTime;
+
+                if (onPointerEnterCounter >= onPointerClickTime)
+                {
+                    _gazedAtObject?.SendMessage("OnPointerClick");
+
+                    Loader.SetActive(false);
+                    SliderObject.value = 0f;
+                }
+            } else {
+                onPointerEnterCounter = 0;
 
                 Loader.SetActive(false);
                 SliderObject.value = 0f;
             }
-        } else {
-            onPointerEnterCounter = 0;
-
-            Loader.SetActive(false);
-            SliderObject.value = 0f;
         }
     }
 
